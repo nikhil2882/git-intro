2d7a04525450846bf19605d5506b8fa716663071
Nikhil Sengar <nikhilsenger94@gmail.com>
2019-08-26 01:24:25 +0530

    added files

---
+++ index.js
@@ -0,0 +1,14 @@
+var welcome_container = document.getElementById("welcome_container");
+
+var login_form = document.getElementById("login_form");
+var user_register = document.getElementById("user_register");
+
+var user_login = document.getElementById("user_login");
+
+user_login.addEventListener( "click", function()
+{
+
+  welcome_container.style.display = "none";
+  login_form.style.display = "block";
+
+});
