1b8f267ec9ef33aecae8e60cb79e57b22c09c4fb
Nikhil Sengar <nikhilsenger94@gmail.com>
2019-08-26 02:02:03 +0530

    change3

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ var user_register = document.getElementById("user_register");
 
 var user_login = document.getElementById("user_login");
 
-user_login.onClick = function()
+user_login.addEventListener( "click", function()
 {
 
   welcome_container.style.display = "none";
-  login_form.style.display = "block";
+  login_form.style.display = "none";
 
-};
+});
 
 
 /**
